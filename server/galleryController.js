4809c54ddf516eeb95b1377cb0b28d546a948a00
Jyotirmoy Roy <78967360+ItsRoy69@users.noreply.github.com>
2022-05-20 19:30:52 +0530

    Delete server directory

--- server/galleryController.js
+++
@@ -1,3 +0,0 @@
-exports.addImage=(req,res)=>{
-  
-}
--- server/galleryRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const { addImage } = require('./galleryController')
-
-router.post('/add/photo', addImage);
-
-module.exports = router;
